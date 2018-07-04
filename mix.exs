162e208cc436d4fe39eed3d99c01463346933897
M.Valitov <marselvalitov@gmail.com>
2018-07-04 14:26:12 +0300

    up ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoneNumber.Mixfile do
   def project do
     [
       app: :phone_number,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       homepage_url: "https://hexdocs.pm/phone_number",
